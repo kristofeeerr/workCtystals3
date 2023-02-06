2b45cb80b4388b91b4d1ff4491c5a15827944fd3
kristofeeerr <123837931+kristofeeerr@users.noreply.github.com>
2023-02-06 14:02:28 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workCrystals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int crystalPrice = 15;
+            
+            Console.WriteLine($" Добро пожаловать в магазин! Сегодня один кристалл стоит {crystalPrice} золотых монет:");
+            Console.Write($"Сколько у вас золотых монет: " );
+            int gold = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write($"Сколько кристаллов вам нужно:");
+            int crystals = Convert.ToInt32(Console.ReadLine());
+
+            gold -= crystalPrice * crystals;
+            Console.WriteLine($"У вас {crystals} кристаллов и {gold} золотых монет");
+        }
+    }
+}
